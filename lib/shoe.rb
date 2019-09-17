55b51329e10780b08456ae7d258a14c40e9b7679
Sarah H <sarahrose.henderson@gmail.com>
2019-09-17 16:40:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,10 @@
 class Shoe
-attr_accessor :color, :size
+attr_accessor :color, :size, :material
 
   def initialize(brand)
     @brand = brand
     @size = size
+    @material = material
   end
   def brand
     @brand
@@ -16,3 +17,4 @@ end
 
 my_shoe = Shoe.new("Nike")
 my_shoe.size = 9.5
+my_shoe.material = "suede"
