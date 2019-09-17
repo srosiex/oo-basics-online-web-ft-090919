3d9184de4622fccc88fed2d7e128fcba65886a4e
Sarah H <sarahrose.henderson@gmail.com>
2019-09-17 16:17:58 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,11 +2,11 @@ class Book
   def initialize(title)
     @title = title
   end
-  def title=(title)
-    @title = title
-  end
-  # def title
-  #   @title
+  # def title=(title)
+  #   @title = title
   # end
+  def title
+    @title
+  end
 end
 Book.new("And Then There Were None")
